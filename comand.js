48027fa5068a93ada7144749a29f472b9f0cf5e8
D4RK08 <110988096+D4RK08@users.noreply.github.com>
2024-01-22 22:28:22 +0100

    Add files via upload

---
+++ comand.js
@@ -0,0 +1,44 @@
+async function comand(){
+    comando_principale = document.getElementById("span-0").textContent;
+    if (comando_principale == "color"){
+        secondo_comando = document.getElementById("span-1").textContent
+        if (secondo_comando=="a"){
+            var body = document.body;
+            body.style.color = "green";
+            stampa("verde");
+        }else if(secondo_comando=="b"){
+            var body = document.body;
+            body.style.color = "white";
+        }
+        
+    }else if (comando_principale== "su"){
+        
+        utent = "root";
+        
+    }else if (comando_principale== "exit"){
+        if (utent == "root"){
+            utent = "user";
+        }else{
+            window.close();
+        }
+        
+    }else if (comando_principale == "loader"){
+        stampa("Loading");
+        await sleep(1000);
+        rimuovi();
+        stampa("Loading.");
+        await sleep(1000);
+        rimuovi();
+        stampa("Loading..");
+        await sleep(1000);
+        rimuovi();
+        stampa("Loading...");
+        await sleep(1000);
+        rimuovi();
+        
+    }else{
+        stampa("Comando non riconosciuto");
+    }
+    new_session();
+}
+
